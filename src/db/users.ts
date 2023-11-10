7da9ff8b0e1c7f5fcae83530454915444e058ba6
MarcinMG-web <gierada.marcin@gmail.com>
2023-11-10 17:47:55 +0100

    Creating User Model

---
+++ src/db/users.ts
@@ -0,0 +1,44 @@
+import mongoose, { trusted } from 'mongoose';
+
+/**
+ * Schema
+ */
+const UserSchema = new mongoose.Schema({
+  userName: { type: String, required: true },
+  email: { type: String, required: true },
+  authentication: {
+    password: { type: String, required: trusted, selected: false },
+    salt: { type: String, select: false },
+    sessionToken: { type: String, select: false },
+  },
+});
+
+/**
+ * Model
+ */
+export const UserModel = mongoose.model('User', UserSchema);
+
+/**
+ * Actions
+ */
+export const getUsers = () => UserModel.find();
+
+// Authentication
+export const getUsersByEmail = (email: string) => UserModel.findOne({ email });
+
+export const getUserBySessionToken = (sessionToken: string) =>
+  UserModel.findOne({
+    'authentication.sessionToken': sessionToken,
+  });
+
+// Interaction on User
+export const getUserById = (id: string) => UserModel.findById(id);
+
+export const createUsers = (values: Record<string, string>) =>
+  new UserModel(values).save().then((user) => user.toObject());
+
+export const deleteUserById = (id: string) =>
+  UserModel.findOneAndDelete({ _id: id });
+
+export const updateUserById = (id: string, values: Record<string, string>) =>
+  UserModel.findByIdAndUpdate(id, values);
