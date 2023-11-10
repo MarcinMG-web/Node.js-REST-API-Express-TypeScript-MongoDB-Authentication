6ac2ab91e0fe4565e19a45adc8fdf9377f188276
MarcinMG-web <gierada.marcin@gmail.com>
2023-11-10 10:48:32 +0100

    init commit

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import bodyParser from 'body-parser';
+import compression from 'compression';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import express from 'express';
+import http from 'http';
+
+const app = express();
+const port = 8080;
+
+app.use(
+  cors({
+    credentials: true,
+  })
+);
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+const server = http.createServer(app);
+
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
