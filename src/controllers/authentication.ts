2b0af80c0e9b78c93d2f2e37694c8f7e7f4b9be6
MarcinMG-web <gierada.marcin@gmail.com>
2023-11-14 15:21:04 +0100

    added login

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -4,6 +4,56 @@ import { getUserByEmail, createUser } from '../db/users';
 import { authentication, random } from '../helpers';
 
 /**
+ * Login
+ */
+
+export const login = async (req: express.Request, res: express.Response) => {
+  try {
+    const { email, password } = req.body;
+
+    // Check provided value
+    if (!email || !password) {
+      return res.sendStatus(400);
+    }
+
+    const user = await getUserByEmail(email).select(
+      '+authentication.salt +authentication.password'
+    );
+
+    if (!user) {
+      return res.sendStatus(400);
+    }
+
+    // Authentication user
+    const expectedHash = authentication(user.authentication.salt, password);
+    if (user.authentication.password !== expectedHash) {
+      return res.sendStatus(403);
+    }
+
+    // Updated User session token
+    const salt = random();
+    user.authentication.sessionToken = authentication(
+      salt,
+      user._id.toString()
+    );
+
+    await user.save();
+
+    // Set the cookie
+    res.cookie('AUTHORIZATION-COOKIE', user.authentication.sessionToken, {
+      domain: 'localhost',
+      path: '/',
+    });
+
+    return res.status(200).json(user).end()
+
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(400);
+  }
+};
+
+/**
  * Register
  */
 export const register = async (req: express.Request, res: express.Response) => {
@@ -34,7 +84,7 @@ export const register = async (req: express.Request, res: express.Response) => {
 
     return res.status(200).json(user).end();
   } catch (error) {
-    console.log(error);
+    console.error(error);
     return res.sendStatus(400);
   }
 };
