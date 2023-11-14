d532ff772f3ef2824a6035a27fc388386c76ffba
MarcinMG-web <gierada.marcin@gmail.com>
2023-11-14 21:11:23 +0100

    added authentication to possible getAllUsers when user is login

--- src/router/users.ts
+++ src/router/users.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { getAllUsers } from '../controllers/users';
+import { isAuthenticated } from '../middlewares';
 
 export default (router: Router) => {
-  router.get('/users', getAllUsers);
+  router.get('/users', isAuthenticated, getAllUsers);
 };
